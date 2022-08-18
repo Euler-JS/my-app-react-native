dfc284def183303bd19d7a161de1820dfc2fd2a8
Euler-JS <joaoalbertojose96@gmail.com>
2022-08-18 14:33:30 +0200

    all config

--- App.js
+++ App.js
@@ -4,7 +4,16 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Text style={styles.tBold}>Hello Jesus</Text>
+      </View>
+      <View style={styles.body}>
+        <Text>Body </Text>
+      </View>
+      <Text style={styles.text}>Euler JS on React Native</Text>
+
+      <Text style={styles.text}>2022</Text>
+      <Text style={styles.text2}>EquipMoz</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +26,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  text:{
+    fontSize:44,
+    color: '#60A3D9',
+  },
+  text2:{
+    fontSize:140,
+    color: '#050A30',
+  },
+  header:{
+    backgroundColor: 'pink',
+    padding: 20,
+    width: '100%',
+  },
+  body:{
+    backgroundColor: 'yellow',
+    width: '100%'
+  },
+  tBold:{
+    fontWeight: 'bold'
+  }
 });
