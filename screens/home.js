0ef915a37ab0bc7b5f46d72c971125eb2310054a
Euler-JS <joaoalbertojose96@gmail.com>
2022-08-25 12:54:59 +0200

    Navegar entre paginas

--- screens/home.js
+++ screens/home.js
@@ -1,11 +1,19 @@
-import { View, Text} from 'react-native';
+import { View, Text, Button} from 'react-native';
 import { globalStyles } from '../styles/global';
 
-export default function Home()
+export default function Home({ navigation })
 {
+    const irParaReview = () =>
+    {
+        navigation.navigate('Review Details')
+        // navigation.push('Review Details')
+        // navigation.pop('Review Details')
+    }
+
     return (
         <View style={globalStyles.container}>
             <Text style={globalStyles.titletext}>Home - Euler JS</Text>
+            <Button title='Go to Review' onPress={irParaReview}/>
         </View>
     )
 }
--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,11 +1,16 @@
-import { View, Text} from 'react-native';
+import { View, Text, Button} from 'react-native';
 import { globalStyles } from '../styles/global';
 
-export default function ReviewDetails()
+export default function ReviewDetails({ navigation })
 {
+    const voltar = () =>
+    {
+        navigation.goBack()
+    }
     return (
         <View style={globalStyles.container}>
             <Text style={globalStyles.titletext}>ReviewDetails Screen</Text>
+            <Button title='Go to Review' onPress={voltar}/>
         </View>
     )
 }
