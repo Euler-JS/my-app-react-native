3fe07046c3d92a1fcd758206d7c19ad44d946e0e
Euler-JS <joaoalbertojose96@gmail.com>
2022-08-25 14:16:33 +0200

    Passar dado entre paginas

--- screens/home.js
+++ screens/home.js
@@ -1,4 +1,5 @@
-import { View, Text, Button} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Button, FlatList, TouchableOpacity} from 'react-native';
 import { globalStyles } from '../styles/global';
 
 export default function Home({ navigation })
@@ -10,10 +11,24 @@ export default function Home({ navigation })
         // navigation.pop('Review Details')
     }
 
+    const  [reviews, setReview] = useState([
+        { title: 'Zelda, Breath of Fresh Air', rating: 5, body: "ll ll ll l", key:'1'},
+        { title: 'lucas, Bqqqq', rating: 4, body: "wwwwq vvv ", key:'2'},
+        { title: 'Servo, Breath of Fresh Air', rating: 2, body: "aaad rdd", key:'3'},
+    ])
+
     return (
         <View style={globalStyles.container}>
             <Text style={globalStyles.titletext}>Home - Euler JS</Text>
             <Button title='Go to Review' onPress={irParaReview}/>
+            <FlatList 
+                data={reviews}
+                renderItem={({ item }) =>(
+                    <TouchableOpacity onPress={()=> navigation.navigate('Review Details', item)}>
+                        <Text style={globalStyles.titletext}>{ item.title }</Text>
+                    </TouchableOpacity>
+                )}
+            />
         </View>
     )
 }
--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,15 +1,20 @@
 import { View, Text, Button} from 'react-native';
 import { globalStyles } from '../styles/global';
 
-export default function ReviewDetails({ navigation })
+export default function ReviewDetails({ route, navigation })
 {
+    const { title, rating, body, key } = route.params;
+    console.log(route.params);
     const voltar = () =>
     {
-        navigation.goBack()
+        // navigation.goBack()
+        console.log(route.params);
     }
     return (
         <View style={globalStyles.container}>
-            <Text style={globalStyles.titletext}>ReviewDetails Screen</Text>
+            <Text style={globalStyles.titletext}>{JSON.stringify(title)}</Text>
+            <Text style={globalStyles.titletext}>{JSON.stringify(body)}</Text>
+            <Text style={globalStyles.titletext}>{JSON.stringify(rating)}</Text>
             <Button title='Go to Review' onPress={voltar}/>
         </View>
     )
